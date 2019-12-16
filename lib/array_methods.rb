99be69e22482cae092dec7c732069a3ca0e72b7c
Simo Khouili <m.simokhouili@gmail.com>
2019-12-16 02:08:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,36 @@
 def using_include(array, element)
-
 end
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.include?("wow")
+array.include?("Tardis")
 
-def using_sort(array)
-
-end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
-end
+#
+# def using_sort(array)
+# array = ["wow", "I", "am", "really", "learning", "arrays!"]
+# end
+# array.sort
+#
+#
+# def using_reverse(array)
+#
+# end
+# array = ["wow", "I", "am", "really", "learning", "arrays!"]
+# array.reverse
+#
+# def using_first(array)
+#
+# end
+# array = ["wow", "I", "am", "really", "learning", "arrays!"]
+# array.first
+#
+# def using_last(array)
+#
+# end
+# array = ["wow", "I", "am", "really", "learning", "arrays!"]
+# array.last
+#
+# def using_size(array)
+#
+# end
+# array = ["wow", "I", "am", "really", "learning", "arrays!"]
+# array.size
